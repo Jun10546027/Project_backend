467b5eeb771aa21d2fa1bff22b7b6d7380d36c44
10546027Jun <10546027@ntub.edu.tw>
2019-05-23 02:02:19 +0800

    '還原程式'

--- ProjectDB/MangerDB/models.py
+++ ProjectDB/MangerDB/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Item(models.Model):
     id = models.CharField(max_length=20,primary_key=True)
@@ -7,4 +8,31 @@ class Item(models.Model):
     MyClass = models.CharField(max_length=20)
     content = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.title
 
+class User(models.Model):
+    # id = models.CharField(max_length=20)
+    LIKES = (
+        ('差評','bad'),
+        ('中等','normal'),
+        ('好','good'),
+    )
+    like = models.CharField(max_length=1,choices=LIKES)
+    name = models.CharField(max_length=20)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+#搜尋 ID
+def search_id(**kwargs):
+    id = kwargs.get('id')
+    try:
+        if id:
+            result = Item.objects.extra(where=['id = %s'],params = [id])
+        else:
+            result = Item.objects.all()
+    except Exception as e:
+        return e.args
+    return result
--- ProjectDB/ProjectDB/urls.py
+++ ProjectDB/ProjectDB/urls.py
@@ -1,23 +1,21 @@
-"""ProjectDB URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+    
 from django.contrib import admin
-from django.urls import path
-from MangerDB.views import homepage
+from django.urls import path,include,re_path
+from ProjectDB.views import homepage
+from MangerDB.views import detail
+from MangerDB.viewset import DataViewSet
+
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'data',DataViewSet)
+
 
 urlpatterns = [
-    path('' , homepage),
+    path('', homepage),
+    path('detail/<str:post_id>/',detail),
+    path('api/',include(router.urls)),
+    path("api-auth.",include('rest_framework.urls',namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
